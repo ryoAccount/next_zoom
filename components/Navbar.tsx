b3ccbacfadee30e46750dd029293b9b766146289
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-04-10 01:56:11 +0900

    until 1:07:35

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignIn, SignUp, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -18,7 +19,9 @@ const Navbar = () => {
       </Link>
 
       <div className='flex-between gap-5'>
-        {/* Clerk - User management */}
+        <UserButton />
+        <SignIn />
+        <SignUp />
         <MobileNav />
       </div>
     </nav>
