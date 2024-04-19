c148ceb0a176cab43eb5cd040b1c4318201ca6a5
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-04-20 06:47:21 +0900

    until 3:01:20

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,10 +1,24 @@
 import { cn } from '@/lib/utils';
-import { CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LayoutList, Users } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+import EndCallButton from './EndCallButton';
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get('personal');
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
   const [showParticipants, setShowParticipants] = useState(false);
 
@@ -30,6 +44,38 @@ const MeetingRoom = () => {
         </div>
       </div>
 
+      <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+        <CallControls />
+
+        <DropdownMenu>
+
+          <div className='flex items-center'>
+            <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+              <LayoutList size={20} className='text-white' />
+            </DropdownMenuTrigger>
+          </div>
+
+          <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
+            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem className='cursor-pointer' onClick={() => {
+                  setLayout(item.toLowerCase() as CallLayoutType)
+                }}>
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className='border-dark-1'/>
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
+            <Users size={20} className='text-white' />
+          </div>
+        </button>
+        {!isPersonalRoom && <EndCallButton />}
+      </div>
     </section>
   )
 }
