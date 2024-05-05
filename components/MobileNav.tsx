ee206fb524110cff1dec697531c80cf08d3d885c
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-05-05 22:54:51 +0900

    ver up and text repair

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -30,13 +30,13 @@ const MobileNav = () => {
         <SheetContent side='left' className='border-none bg-dark-1'>
           <Link href='/' className='flex items-center gap-1'>
             <Image 
-              src="/icons/logo.svg"
+              src="/icons/ryoom-logo.svg"
               width={32}
               height={32}
-              alt='Yoom logo'
+              alt='Ryoom logo'
               className='max-sm:size-10'
             />
-            <p className='text-[26px] font-extrabold text-white'>Yoom</p>
+            <p className='text-[26px] font-extrabold text-white'>Ryoom</p>
           </Link>
           <div className='flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto'>
             <SheetClose asChild>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,13 +9,13 @@ const Navbar = () => {
     <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href='/' className='flex items-center gap-1'>
         <Image 
-          src="/icons/logo.svg"
+          src="/icons/ryoom-logo.svg"
           width={32}
           height={32}
-          alt='Yoom logo'
+          alt='Ryoom logo'
           className='max-sm:size-10'
         />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Ryoom</p>
       </Link>
 
       <div className='flex-between gap-5'>
